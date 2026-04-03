0e87e42abc2c0c0f4f6075ea93b04b28c902d166
Stephen Pollett <stephen.pollett@gmail.com>
2026-04-03 13:39:34 -0230

    Cool Sorting

--- day3/src/main.cpp
+++ day3/src/main.cpp
@@ -1,9 +1,73 @@
 #include "main.h"
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <vector>
+
+bool cellCompareGreaterPosition(const cell a, const cell b) {
+  return a.position > b.position;
+}
 
 int main(int argc, char *argv[]) {
-  std::printf("Hello World\r\n");
-  std::printf(readFileContents("tests/test.txt").c_str());
+  std::string inputFile = readFileContents("tests/test.txt");
+  // std::printf(inputFile.c_str());
+  batteryBank bank = generateBatteryBank(inputFile.c_str());
+  std::printf("The First Value in the Bank is: %i\n", bank.battery[0][0].volatage);
+  int numberOfBatteriesToPull = 2;
+  int total = 0;
+  for (std::vector<cell> battery : bank.battery) {
+    std::printf("%i\n", battery[0].volatage);
+    cell cellsToEnable[numberOfBatteriesToPull];
+    for (int i = 0; i < numberOfBatteriesToPull; i++) {
+      cellsToEnable[i] = battery[i];
+    }
+    int n = sizeof(cellsToEnable) / sizeof(cellsToEnable[0]);
+    std::sort(cellsToEnable, cellsToEnable + n, cellCompareGreaterPosition);
+    int sum = 0;
+    for (int i = 0; i < numberOfBatteriesToPull; i++) {
+      sum += cellsToEnable[i].volatage * std::pow(10, i);
+    }
+    std::printf("Test Value: %i\n", sum);
+    total += sum;
+  }
+  std::printf("The total volatage is: %i\n", total);
   return 0;
+}
+
+bool cellGreaterCompareVoltages(const cell a, const cell b) {
+  return a.volatage > b.volatage;
+};
+
+batteryBank generateBatteryBank(const char *fileToMakeBattery) {
+  batteryBank bank;
+  std::stringstream parser = std::stringstream(fileToMakeBattery);
+  std::string line;
+  std::regex numbersOnly = std::regex("^[^0-9]+");
+  if (fileToMakeBattery != NULL) {
+    while (std::getline(parser, line, '\n')) {
+      if (std::regex_match(line, numbersOnly)) {
+        continue;
+      }
+      std::vector<cell> batteryLineHolder = std::vector<cell>();
+      std::printf(line.c_str());
+      std::printf("\n");
+      cell cellHolder;
+      for (int pos = 0; pos < line.size() - 1; pos++) {
+        cellHolder.position = pos;
+        cellHolder.volatage = line[pos] - '0';
+	//std::printf("The state of the cell is: volt: %i pos: %i\n", cellHolder.volatage, cellHolder.position);
+        batteryLineHolder.push_back(cellHolder);
+      }
+      std::sort(batteryLineHolder.begin(), batteryLineHolder.end(),
+                cellGreaterCompareVoltages);
+      bank.battery.push_back(batteryLineHolder);
+    }
+  }
+  return bank;
 }
 
 std::string readFileContents(const char* fileName) {
--- day3/src/main.h
+++ day3/src/main.h
@@ -4,6 +4,18 @@
 #include <cstdio>
 #include <fstream>
 #include <string>
+#include <vector>
+
+typedef struct cell {
+  int volatage = 0;
+  int position = 0;
+} cell;
+
+typedef struct batteryBank {
+  std::vector<std::vector<cell>> battery = std::vector<std::vector<cell>>();
+} batteryBank;
+
+batteryBank generateBatteryBank(const char *fileToMakeBattery);
 
 std::string readFileContents(const char *fileName);
 
